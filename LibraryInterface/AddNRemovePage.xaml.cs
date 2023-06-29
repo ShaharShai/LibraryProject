3cd8c5154a6fa85bc5db8ae9e37e61c5309a1f37
ShaharShai <138113516+ShaharShai@users.noreply.github.com>
2023-06-29 14:30:22 +0300

    Update AddNRemovePage.xaml.cs

--- LibraryInterface/AddNRemovePage.xaml.cs
+++ LibraryInterface/AddNRemovePage.xaml.cs
@@ -17,13 +17,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Manager = LibraryLogic.Manager;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Library_Project01
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
 
     public sealed partial class AddNRemovePage : Page
     {
